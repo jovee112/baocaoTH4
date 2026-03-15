bb7631b1e11c1f35bbc9b3472c2ad14e65a7b6cd
jovee112 <2351060477@e.tlu.edu.vn>
2026-03-15 15:23:23 +0700

    Chỉnh sửa lại cấu trúc file product_models

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -5,6 +5,9 @@ class ProductModel {
   final String description;
   final String category;
   final String image;
+  // Bổ sung thêm các field để phục vụ UI nâng cao của đề bài
+  final double? rate;
+  final int? count;
 
   const ProductModel({
     required this.id,
@@ -13,6 +16,8 @@ class ProductModel {
     required this.description,
     required this.category,
     required this.image,
+    this.rate,
+    this.count,
   });
 
   factory ProductModel.fromJson(Map<String, dynamic> json) {
@@ -23,6 +28,11 @@ class ProductModel {
       description: json['description'] as String,
       category: json['category'] as String,
       image: json['image'] as String,
+      // Lấy thêm rating từ FakeStore API để làm phần "Đã bán" hoặc "Sao"
+      rate: json['rating'] != null
+          ? (json['rating']['rate'] as num).toDouble()
+          : 0.0,
+      count: json['rating'] != null ? json['rating']['count'] as int : 0,
     );
   }
 
